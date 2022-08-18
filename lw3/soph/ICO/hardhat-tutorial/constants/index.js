4f75513fa9ff2fd548d7698c8c063b833dfbef57
thishitfake <solarlunarpunk@gmail.com>
2022-08-17 22:30:41 -0500

    sub module wokrin

---
+++ lw3/soph/ICO/hardhat-tutorial/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0x11dA75a325F0a38C6dC135015bb4508Bd2d3e306";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
