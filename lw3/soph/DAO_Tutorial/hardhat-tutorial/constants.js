fc16138ee8cf8fd2eb3f57292aa311cbb03e3df4
thishitfake <solarlunarpunk@gmail.com>
2022-08-17 21:36:43 -0500

    big push

---
+++ lw3/soph/DAO_Tutorial/hardhat-tutorial/constants.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0x11dA75a325F0a38C6dC135015bb4508Bd2d3e306";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
