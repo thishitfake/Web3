55f4d1d04f9574279a6386609ad28a7f531f37d7
thishitfake <solarlunarpunk@gmail.com>
2022-08-27 01:58:26 -0500

    Goerli dao up

--- lw3/soph/DAO_Tutorial/hardhat-tutorial/constants.js
+++ lw3/soph/DAO_Tutorial/hardhat-tutorial/constants.js
@@ -1,5 +1,5 @@
 // Replace the value with your NFT contract address
 const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
-  "0x11dA75a325F0a38C6dC135015bb4508Bd2d3e306";
+  "0x8C12D7b116F1476D65c229b9c0c238a6Fa147f43";
 
 module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
